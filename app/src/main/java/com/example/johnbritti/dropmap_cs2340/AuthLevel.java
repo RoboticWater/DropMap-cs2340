90d6e27a156a01387882b1fa7df1402cd7efd38d
RoboticWater <jakebritti@gmail.com>
2017-02-09 12:33:42 -0500

    Added skeleton of User class/AuthLevel

---
+++ app/src/main/java/com/example/johnbritti/dropmap_cs2340/AuthLevel.java
@@ -0,0 +1,14 @@
+package com.example.johnbritti.dropmap_cs2340;
+
+import java.io.Serializable;
+
+/**
+ * Created by johnbritti on 2/9/17.
+ */
+
+public enum AuthLevel {
+    USER,
+    WORKER,
+    MANAGER,
+    ADMINISTRATOR;
+}
