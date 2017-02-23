b2b87a2ad8961aaa34c10387886dbf64872d0087
RoboticWater <jakebritti@gmail.com>
2017-02-23 17:09:04 -0500

    Added water report edit screen

---
+++ app/src/main/java/com/dropmap_cs2340/EditWaterReport.java
@@ -0,0 +1,13 @@
+package com.dropmap_cs2340;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditWaterReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_water_report);
+    }
+}
