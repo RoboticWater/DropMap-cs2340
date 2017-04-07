588ba6432003f0c26840a45a5965cec4cdc39755
John Britti <jakebritti@gmail.com>
2017-04-06 21:42:08 -0400

    Graph handles location

--- app/src/main/java/com/dropmap_cs2340/HistoryForm.java
+++ app/src/main/java/com/dropmap_cs2340/HistoryForm.java
@@ -6,54 +6,45 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+/**
+ * Displays the history PPM of a location
+ */
 public class HistoryForm extends AppCompatActivity {
 
-    public static final int GRAPH_ANIMATE_MILLIS = 5000;
+    public static final int GRAPH_ANIMATE_MILLIS = 1000;
     public static final int THE_BEGINNING_OF_TIME = 1970;
-    public static final int MAX_VIRUS_PPM = 300;
-    public static final int MAX_CONTAMINANT_PPM = 300;
+    public static final double LOCATION_DEVIATION = 100;
 
     private RadioButton          contaminantButton;
     private RadioButton          virusButton;
-    private Spinner              reportSpinner;
-    private AutoCompleteTextView yearInput;
+    private EditText             xEdit;
+    private EditText             yEdit;
 
     private List<WaterReport> waterReports;
-    private ArrayList<String> reportNames;
+    private List<String> reportNames;
     private String type = "virus";
-    private WaterReport selectedReport;
     private int currentYear = -1;
 
     private FirebaseDatabase database;
@@ -65,8 +56,9 @@ public class HistoryForm extends AppCompatActivity {
 
         virusButton       = (RadioButton) findViewById(R.id.buttonVirus);
         contaminantButton = (RadioButton) findViewById(R.id.buttonContaminant);
-        reportSpinner     = (Spinner)     findViewById(R.id.reportSpinner);
-        yearInput         = (AutoCompleteTextView) findViewById(R.id.inputYear);
+        xEdit             = (EditText) findViewById(R.id.editLat);
+        yEdit             = (EditText) findViewById(R.id.editLon);
+        AutoCompleteTextView yearInput = (AutoCompleteTextView) findViewById(R.id.inputYear);
 
         ArrayList<String> years = new ArrayList<>();
         currentYear = Calendar.getInstance().get(Calendar.YEAR);
@@ -90,7 +82,6 @@ public class HistoryForm extends AppCompatActivity {
                             reportNames.add(wr.getReportName());
                             waterReports.add(wr);
                         }
-                        makeSpinner();
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
@@ -111,38 +102,35 @@ public class HistoryForm extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start,
                                       int before, int count) {
                 currentYear = Integer.parseInt(s.toString());
-                if ((currentYear >= THE_BEGINNING_OF_TIME) &&
-                        (reportSpinner.getSelectedItem() != null))  {
+                if ((currentYear >= THE_BEGINNING_OF_TIME) && !isLocationEmpty())  {
                     onGraphReady();
                 }
             }
         });
+
+        xEdit.addTextChangedListener(new LocationTextWatcher());
+        yEdit.addTextChangedListener(new LocationTextWatcher());
     }
 
-    private void makeSpinner() {
-        ArrayAdapter<String> reportAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, reportNames);
-        reportAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        reportSpinner.setAdapter(reportAdapter);
+    private class LocationTextWatcher implements TextWatcher {
 
-        reportSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
-                String report = (String) reportSpinner.getSelectedItem();
-                for (WaterReport wr : waterReports) {
-                    if (wr.getReportName().equals(report)) {
-                        selectedReport = wr;
-                    }
-                }
-                if ((currentYear >= THE_BEGINNING_OF_TIME) &&
-                        (reportSpinner.getSelectedItem() != null)) {
-                    onGraphReady();
-                }
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start,
+                                      int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+            Log.d("-----", s.toString() + "|" + isInteger(xEdit) + "|" + isInteger(yEdit));
+            if ((currentYear >= THE_BEGINNING_OF_TIME) && !isLocationEmpty()) {
+                onGraphReady();
             }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {}
-        });
+        }
     }
 
     protected void onGraphReady() {
@@ -156,44 +144,53 @@ public class HistoryForm extends AppCompatActivity {
         xAxis.setValueFormatter(new MyXAxisValueFormatter(months));
         chart.animateY(GRAPH_ANIMATE_MILLIS);
 
-        database.getReference().child("reportHistory").child(selectedReport.getId())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if ("virus".equals(type)) {
-                            chart.getDescription().setText("Average Virus PPM per Month");
-                        } else {
-                            chart.getDescription().setText("Average Contaminant PPM per Month");
-                        }
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            PurityHistory pr = snapshot.getValue(PurityHistory.class);
-                            long date = Long.parseLong(snapshot.getKey());
+        for (WaterReport wr : waterReports) {
+            if (dist(Double.parseDouble(xEdit.getText().toString()),
+                    Double.parseDouble(yEdit.getText().toString()),
+                    wr.getX(), wr.getY()) > LOCATION_DEVIATION) {
+                continue;
+            }
+            database.getReference().child("reportHistory").child(wr.getId())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if ("virus".equals(type)) {
+                                chart.getDescription().setText("Average Virus PPM per Month");
+                            } else {
+                                chart.getDescription().setText("Average Contaminant PPM per Month");
+                            }
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                PurityHistory pr = snapshot.getValue(PurityHistory.class);
+                                long date = Long.parseLong(snapshot.getKey());
 
-                            Calendar c = Calendar.getInstance();
-                            c.setTimeInMillis(date);
-                            int month = c.get(Calendar.MONTH);
-                            int year = c.get(Calendar.YEAR);
-                            if (year == currentYear) {
-                                if ("virus".equals(type)) {
-                                    entries.add(new BarEntry(month, (float)pr.getVirusPPM()));
-                                } else {
-                                    entries.add(new BarEntry(month, (float)pr.getContaminatePPM()));
+                                Calendar c = Calendar.getInstance();
+                                c.setTimeInMillis(date);
+                                int month = c.get(Calendar.MONTH);
+                                int year = c.get(Calendar.YEAR);
+                                if (year == currentYear) {
+                                    if ("virus".equals(type)) {
+                                        entries.add(new BarEntry(month,
+                                                (float) pr.getVirusPPM()));
+                                    } else {
+                                        entries.add(new BarEntry(month,
+                                                (float) pr.getContaminatePPM()));
+                                    }
                                 }
                             }
+                            if (!entries.isEmpty()) {
+                                ScatterDataSet dataSet = new ScatterDataSet(entries, "# of PPM");
+                                ScatterData data = new ScatterData(dataSet);
+                                chart.setData(data);
+                            } else {
+                                chart.clear();
+                            }
                         }
-                        if (!entries.isEmpty()) {
-                            ScatterDataSet dataSet = new ScatterDataSet(entries, "# of PPM");
-                            ScatterData data = new ScatterData(dataSet);
-                            chart.setData(data);
-                        } else {
-                            chart.clear();
-                        }
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+        }
 
     }
 
@@ -219,7 +216,7 @@ public class HistoryForm extends AppCompatActivity {
     public void onVirusClicked(View v) {
         contaminantButton.setChecked(false);
         type = "virus";
-        if ((currentYear >= THE_BEGINNING_OF_TIME) && (reportSpinner.getSelectedItem() != null)) {
+        if ((currentYear >= THE_BEGINNING_OF_TIME) && !isLocationEmpty()) {
             onGraphReady();
         }
     }
@@ -231,8 +228,41 @@ public class HistoryForm extends AppCompatActivity {
     public void onContaminantClicked(View v) {
         virusButton.setChecked(false);
         type = "contaminant";
-        if ((currentYear != -1) && (reportSpinner.getSelectedItem() != null)) {
+        if ((currentYear != -1) && !isLocationEmpty()) {
             onGraphReady();
         }
+    }
+
+    /**
+     * Determines if location inputs are empty
+     * @return boolean whether or not one or more location inputs are empty
+     */
+    public boolean isLocationEmpty() {
+        return !isInteger(xEdit) || !isInteger(yEdit);
+    }
+
+    /**
+     * whether the string input is an integer or not
+     * @return whether the string input is an integer or not
+     */
+    public boolean isInteger(EditText edit) {
+        try {
+            int num = Integer.parseInt(edit.getText().toString());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * returns distance between to points
+     * @param x0 first x coordinate
+     * @param y0 first y coordinate
+     * @param x1 second x coordinate
+     * @param y1 second y coordinate
+     * @return distance between two points
+     */
+    public double dist(double x0, double y0, double x1, double y1) {
+        return Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1- y0, 2));
     }
 }
